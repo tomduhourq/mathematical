6cdc3d48b91985c18910064bbac15490d510025d
Tomás <t.duhourq@gmail.com>
2015-03-15 18:07:41 -0300

    Refactor types + operations

--- src/main/scala/vectors/Vector.scala
+++ src/main/scala/vectors/Vector.scala
@@ -7,25 +7,36 @@ import utils.NumberUtils._
  * and must define the common operators.
  */
 sealed trait Vector {
-  def *(n: Int): Vector
+  def *(n: Double)         : Vector
+  def *(v: Vector)         : Double
+  def magnitude            : Double
+  def projection(v: Vector) =
+    this * v / v.magnitude.square
 }
 /**
  * Vectors in R2
  */
-case class VectorR2(x: Int, y: Int) extends Vector {
+case class VectorR2(x: Double, y: Double) extends Vector {
 
-  def +(that: VectorR2) =
+  def +(that: VectorR2)       =
     VectorR2(x + that.x, y + that.y)
 
-  def -(that: VectorR2) =
+  def -(that: VectorR2)       =
     this + VectorR2(-that.x, -that.y)
 
-  def *(that: Int)      =
+  def *(that: Double)         =
     VectorR2(x * that, y * that)
-  def *(that: VectorR2) =
-    x * that.x + y * that.y
+  def *(that: Vector)       =
+    that match {
+      case VectorR2(a, b) =>
+        x * a + y * b
+      case _              =>
+        throw new RuntimeException(
+          "Cannot multiply the 2 types of Vectors"
+        )
+    }
 
-  def x(that: VectorR2) = {
+  def x(that: VectorR2)       = {
     val u = this.toR3
     val v: VectorR3 = that.toR3
     R3.resolveX(u, v) +
@@ -33,17 +44,20 @@ case class VectorR2(x: Int, y: Int) extends Vector {
     R3.resolveZ(u, v)
   }
 
-  def module            =
+  def magnitude               =
     math.sqrt(x.square + y.square)
 
-  def toR3: VectorR3    =
+  def angle                   =
+    1/math.tan(y/x)
+
+  def toR3: VectorR3          =
     VectorR3(x, y, 0)
 }
 
 /**
  * Vectors in R3
  */
-case class VectorR3(x: Int, y: Int, z: Int) extends Vector {
+case class VectorR3(x: Double, y: Double, z: Double) extends Vector {
 
   def +(that: VectorR3) =
     VectorR3(x + that.x, y + that.y, z + that.z)
@@ -51,16 +65,24 @@ case class VectorR3(x: Int, y: Int, z: Int) extends Vector {
   def -(that: VectorR3) =
     this + VectorR3(-that.x, -that.y, -that.z)
 
-  def *(that: VectorR3) =
-    x * that.x + y * that.y + z * that.z
-  def *(that: Int)      =
+  def *(that: Vector)       =
+    that match {
+      case VectorR3(a, b, c) =>
+        x * a + y * b + z * c
+      case _                 =>
+        throw new RuntimeException(
+          "Cannot multiply the 2 types of Vectors"
+        )
+    }
+  def *(that: Double)      =
     VectorR3(x * that, y * that, z * that)
 
-  def module =
+  def magnitude         =
     math.sqrt(x.square + y.square + z.square )
 }
 
 object R3 {
+
   lazy val i = VectorR3(1, 0, 0)
   lazy val j = VectorR3(0, 1, 0)
   lazy val k = VectorR3(0, 0, 1)
@@ -68,7 +90,7 @@ object R3 {
   def resolveX(u: VectorR3, v: VectorR3) =
     i * (u.y * v.z - (v.y * u.z))
   def resolveY(u: VectorR3, v: VectorR3) =
-    (j * -1) * (u.x * v.z - (v.x * u.z))
+    (j * -1.0) * (u.x * v.z - (v.x * u.z))
   def resolveZ(u: VectorR3, v: VectorR3) =
     k * (u.x * v.y - (v.x * u.y))
 }
--- src/test/scala/vectors/VectorsTest.scala
+++ src/test/scala/vectors/VectorsTest.scala
@@ -3,9 +3,6 @@ package vectors
 import org.scalatest.{FreeSpec, Matchers}
 import utils.NumberUtils._
 
-/**
- * Created by tomas on 14/03/15.
- */
 class VectorsTest extends FreeSpec with Matchers {
 
   "My little vector library" - {
@@ -14,6 +11,7 @@ class VectorsTest extends FreeSpec with Matchers {
         val u = VectorR2(2, 5)
         val v = VectorR2(3, 4)
         val w = VectorR2(-300, 1)
+        val x = VectorR2(1,180)
         "Adds two vectors" in {
           u + v should be (VectorR2(5, 9))
           v + w should be (VectorR2(-297, 5))
@@ -37,9 +35,18 @@ class VectorsTest extends FreeSpec with Matchers {
           w x v should be (VectorR3(0, 0, -1203))
         }
         "Resolves modules" in {
-          u.module should be (math.sqrt(29))
-          v.module should be (5)
-          w.module should be (math.sqrt(90001))
+          u.magnitude should be (math.sqrt(29))
+          v.magnitude should be (5)
+          w.magnitude should be (math.sqrt(90001))
+        }
+        "Computes the angle formed by its coordinates" in {
+          u.angle should be (-1.3386481283041514)
+          w.angle should be (-299.99888888806584)
+          x.angle should be (0.7469988144140444)
+        }
+        "Projects correctly 2 vectors" in {
+          u.projection(v) should be (1.04)
+          v.projection(u) should be (0.8965517241379312)
         }
       }
     }
